68cfa8cdb07ae38090c5d5c984c55511d304b8d7
ravi <50861988+ravi-hle@users.noreply.github.com>
2023-05-04 09:26:41 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React library V2.1
+          Learn React library V2.2
         </a>
       </header>
     </div>
